b019175a54a974175ab4567aecec5af793d06615
TranDo25 <73243952+TranDo25@users.noreply.github.com>
2023-08-30 08:54:50 +0700

    add project vuejs learning in udemy

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>this page could not be found - maybe check all our <router-link to="/coaches">coaches</router-link></p>
+        </base-card>
+    </section>
+</template>
